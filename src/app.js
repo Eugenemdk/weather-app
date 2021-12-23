aef15b6402e3aba9c7806bac7da466de0b83b43f
Yevhen <medyanik.jenya@gmail.com>
2021-12-23 22:07:57 +0200

    gbs error fixed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express=require("express")
 const path=require("path")
-const hbs=require("handlebars")
 const res = require("express/lib/response")
 const request=require("postman-request")
 const geocode=require("./utils/geocode")
